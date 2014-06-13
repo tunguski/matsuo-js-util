1b241f30db278a56be0a815b5807c6ea93f52291
marek <marek.romanowski@gmail.com>
2014-06-13 11:36:16 +0200

    Fix condition

--- matsuo-js-util.js
+++ matsuo-js-util.js
@@ -182,17 +182,16 @@ var matsuo_js_util = {};
     }
   };
 
-  matsuo_js_util.array_filter = function(fun /*, thisp*/) {
+  matsuo_js_util.array_filter = function(fun) {
     var len = this.length;
-    if (typeof fun != "function")
+    if (!_.isFunction(fun)) {
       throw new TypeError();
+    }
 
     var res = new Array();
     var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in this)
-      {
+    for (var i = 0; i < len; i++) {
+      if (i in this) {
         var val = this[i]; // in case fun mutates this
         if (fun.call(thisp, val, i, this))
           res.push(val);
